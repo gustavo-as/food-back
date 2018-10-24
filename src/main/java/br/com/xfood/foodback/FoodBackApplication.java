4eb87536d2b877e5b5e0df3e5fe5ee266cfc4dc0
gustavo <gustavo.simoes@hotmail.com.br>
2018-10-24 20:06:00 -0300

    First Commit

---
+++ src/main/java/br/com/xfood/foodback/FoodBackApplication.java
@@ -0,0 +1,12 @@
+package br.com.xfood.foodback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodBackApplication.class, args);
+    }
+}
